52722e1ac3bd404751eb2e75d1a5a68c6c4484b7
Mitchell Rosen <mitchell@simspace.com>
2021-02-21 09:02:39 -0500

    mit commit

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Control.Exception (throwIO)
+import Control.Monad
 import Data.Char
 import Data.Text (Text)
 import Data.Text qualified as Text
@@ -13,12 +15,28 @@ main :: IO ()
 main =
   getArgs >>= \case
     ["clone", parseGitRepo . Text.pack -> Just (url, name)] -> do
-      (out, err, code) <- git "clone" [url, "--separate-git-dir", name <> "/.git", name <> "/master"]
-      print (out, err, code)
+      (_out, _err, code) <- git ["clone", url, "--separate-git-dir", name <> "/.git", name <> "/master"]
+      when (code /= ExitSuccess) (throwIO code)
+    ["commit"] -> do
+      branch <- do
+        ([branch], _err, code) <- git ["branch", "--show-current"]
+        when (code /= ExitSuccess) (throwIO code)
+        pure branch
+      do
+        (_out, _err, code) <- git ["add", "--all"]
+        when (code /= ExitSuccess) (throwIO code)
+      do
+        code <- git2 ["commit", "--all"]
+        when (code /= ExitSuccess) (throwIO code)
+      do
+        (_out, _err, code) <- git ["push", "origin", branch <> ":" <> branch]
+        when (code /= ExitSuccess) (throwIO code)
+    -- TODO pop stash
     _ ->
       (Text.putStrLn . Text.unlines)
         [ "Usage:",
-          "  mit clone ≪repo≫"
+          "  mit clone ≪repo≫",
+          "  mit commit"
         ]
 
 -- git@github.com:mitchellwrosen/mit.git -> Just ("git@github.com:mitchellwrosen/mit.git", "mit")
@@ -27,20 +45,22 @@ parseGitRepo url = do
   url' <- Text.stripSuffix ".git" url
   pure (url, Text.takeWhileEnd (/= '/') url')
 
-git :: Text -> [Text] -> IO ([Text], [Text], ExitCode)
-git command args = do
+--
+
+git :: [Text] -> IO ([Text], [Text], ExitCode)
+git args = do
   do
     let quote :: Text -> Text
         quote s =
           if Text.any isSpace s then "'" <> Text.replace "'" "\\'" s <> "'" else s
-    Text.putStrLn (Text.unwords ("git" : command : map quote args))
+    Text.putStrLn (Text.unwords ("git" : map quote args))
   (Nothing, Just stdoutHandle, Just stderrHandle, processHandle) <-
     createProcess
       CreateProcess
         { child_group = Nothing,
           child_user = Nothing,
           close_fds = True,
-          cmdspec = RawCommand "git" (map Text.unpack (command : args)),
+          cmdspec = RawCommand "git" (map Text.unpack args),
           create_group = False,
           cwd = Nothing,
           delegate_ctlc = False,
@@ -57,6 +77,7 @@ git command args = do
   exitCode <- waitForProcess processHandle
   stdoutLines <- drainTextHandle stdoutHandle
   stderrLines <- drainTextHandle stderrHandle
+  print (stdoutLines, stderrLines, exitCode)
   pure (stdoutLines, stderrLines, exitCode)
   where
     drainTextHandle :: Handle -> IO [Text]
@@ -68,3 +89,34 @@ git command args = do
                 loop (line : acc)
               True -> pure (reverse acc)
       loop []
+
+git2 :: [Text] -> IO ExitCode
+git2 args = do
+  do
+    let quote :: Text -> Text
+        quote s =
+          if Text.any isSpace s then "'" <> Text.replace "'" "\\'" s <> "'" else s
+    Text.putStrLn (Text.unwords ("git" : map quote args))
+  (Nothing, Nothing, Nothing, processHandle) <-
+    createProcess
+      CreateProcess
+        { child_group = Nothing,
+          child_user = Nothing,
+          close_fds = True,
+          cmdspec = RawCommand "git" (map Text.unpack args),
+          create_group = False,
+          cwd = Nothing,
+          delegate_ctlc = True,
+          env = Nothing,
+          new_session = False,
+          std_err = Inherit,
+          std_in = Inherit,
+          std_out = Inherit,
+          -- windows-only
+          create_new_console = False,
+          detach_console = False,
+          use_process_jobs = False
+        }
+  exitCode <- waitForProcess processHandle
+  print exitCode
+  pure exitCode
