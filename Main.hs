a263cb645e814ae7c7a6f6f63313fffa9c15d70e
Mitchell Rosen <mitchell@simspace.com>
2021-02-20 22:09:26 -0500

    mit clone

--- Main.hs
+++ Main.hs
@@ -1,4 +1,70 @@
 module Main where
 
+import Data.Char
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.IO qualified as Text
+import System.Environment (getArgs)
+import System.Exit
+import System.IO (Handle, hIsEOF)
+import System.Process
+
 main :: IO ()
-main = pure ()
+main =
+  getArgs >>= \case
+    ["clone", parseGitRepo . Text.pack -> Just (url, name)] -> do
+      (out, err, code) <- git "clone" [url, "--separate-git-dir", name <> "/.git", name <> "/master"]
+      print (out, err, code)
+    _ ->
+      (Text.putStrLn . Text.unlines)
+        [ "Usage:",
+          "  mit clone ≪repo≫"
+        ]
+
+-- git@github.com:mitchellwrosen/mit.git -> Just ("git@github.com:mitchellwrosen/mit.git", "mit")
+parseGitRepo :: Text -> Maybe (Text, Text)
+parseGitRepo url = do
+  url' <- Text.stripSuffix ".git" url
+  pure (url, Text.takeWhileEnd (/= '/') url')
+
+git :: Text -> [Text] -> IO ([Text], [Text], ExitCode)
+git command args = do
+  do
+    let quote :: Text -> Text
+        quote s =
+          if Text.any isSpace s then "'" <> Text.replace "'" "\\'" s <> "'" else s
+    Text.putStrLn (Text.unwords ("git" : command : map quote args))
+  (Nothing, Just stdoutHandle, Just stderrHandle, processHandle) <-
+    createProcess
+      CreateProcess
+        { child_group = Nothing,
+          child_user = Nothing,
+          close_fds = True,
+          cmdspec = RawCommand "git" (map Text.unpack (command : args)),
+          create_group = False,
+          cwd = Nothing,
+          delegate_ctlc = False,
+          env = Nothing,
+          new_session = False,
+          std_err = CreatePipe,
+          std_in = NoStream,
+          std_out = CreatePipe,
+          -- windows-only
+          create_new_console = False,
+          detach_console = False,
+          use_process_jobs = False
+        }
+  exitCode <- waitForProcess processHandle
+  stdoutLines <- drainTextHandle stdoutHandle
+  stderrLines <- drainTextHandle stderrHandle
+  pure (stdoutLines, stderrLines, exitCode)
+  where
+    drainTextHandle :: Handle -> IO [Text]
+    drainTextHandle handle = do
+      let loop acc =
+            hIsEOF handle >>= \case
+              False -> do
+                line <- Text.hGetLine handle
+                loop (line : acc)
+              True -> pure (reverse acc)
+      loop []
