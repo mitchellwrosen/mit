206503da15ec52c3b9b8b963229cb3155047cdc5
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-01-05 10:10:41 -0800

    remove redundant imports

--- src/Mit/Stanza.hs
+++ src/Mit/Stanza.hs
@@ -1,27 +1,10 @@
 module Mit.Stanza where
 
 import qualified Data.List as List
-import Data.List.NonEmpty (NonEmpty ((:|)))
-import qualified Data.List.NonEmpty as List1
-import qualified Data.Sequence as Seq
-import qualified Data.Text as Text
-import qualified Data.Text.ANSI as Text
-import qualified Data.Text.Builder.ANSI as Text.Builder
-import qualified Data.Text.Encoding.Base64 as Text
 import qualified Data.Text.IO as Text
 import qualified Data.Text.Lazy.Builder as Text (Builder)
-import qualified Data.Text.Lazy.Builder as Text.Builder
 import qualified Mit.Builder as Builder
-import Mit.Clock (getCurrentTime)
-import Mit.Directory
-import Mit.Git
 import Mit.Prelude
-import qualified Mit.Seq as Seq
-import qualified Mit.Seq1 as Seq1
-import Mit.State
-import Mit.Undo
-import System.Environment (getArgs)
-import System.Exit (ExitCode (..), exitFailure)
 
 type Stanza =
   Maybe Text.Builder
