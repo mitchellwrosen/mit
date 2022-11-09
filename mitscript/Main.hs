10c5a98c2ab99dee5271e275d036587b779df29a
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-11-09 12:34:01 +0530

    add initial-commit test

--- mitscript/Main.hs
+++ mitscript/Main.hs
@@ -12,20 +12,29 @@ import System.IO.Error (isEOFError)
 import System.IO.Temp
 import System.Process qualified as Process
 import Prelude hiding (lex)
+import System.Environment
 
 main :: IO ()
 main = do
   withSystemTempDirectory "mitscript" \tempdir -> do
-    putStrLn tempdir
     createDirectory (tempdir </> "remote")
     createDirectory (tempdir </> "remote" </> ".git")
     withCurrentDirectory (tempdir </> "remote" </> ".git") do
-      Process.callCommand "git init --bare"
+      Process.callCommand "git init --bare --quiet"
     createDirectory (tempdir </> "local")
     createDirectory (tempdir </> "local" </> "main")
     withCurrentDirectory (tempdir </> "local" </> "main") do
-      Process.callCommand "git init"
+      Process.callCommand "git init --quiet"
       Process.callCommand ("git remote add origin " ++ (tempdir </> "remote" </> ".git"))
+
+      -- Deterministic commit hashes
+      setEnv "GIT_AUTHOR_NAME" "Bob Ross"
+      setEnv "GIT_AUTHOR_EMAIL" "bob@bobross.com"
+      setEnv "GIT_AUTHOR_DATE" "1970-01-01T00:00:00"
+      setEnv "GIT_COMMITTER_NAME" "Bob Ross"
+      setEnv "GIT_COMMITTER_EMAIL" "bob@bobross.com"
+      setEnv "GIT_COMMITTER_DATE" "1970-01-01T00:00:00"
+
       forlines \line0 -> do
         case parseLine line0 of
           Comment line -> Text.putStrLn line
